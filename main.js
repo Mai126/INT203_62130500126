bac20689e4c6a430d1b8ceaf107523c4346d4bec
Noparat Prasongdee <noparat.saimai@mail.kmutt.ac.th>
2021-02-03 12:19:46 +0700

    work2

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
     const app = {
         data() {
             return {
-                msg: 'Hello, Vue3'
+                firstname: 'Noparat',
+                lastname: 'Prasongdee',
+                msg: '@saimai.126',
+                faculty: 'Information Technology, IT#25',
+                ID: 62130500126,
+                shown: true,
+                url: 'https://www.facebook.com/saimaismile2000/',
+                url2: 'https://www.instagram.com/saimaismile_/',
+                url3: 'https://www.youtube.com/channel/UCJR5tE0WM2_ry9idAxP46eg',
+                image: './images/saimai.jpg',
+            
             }
+        },
+        created(){
+console.log('message is '+this.msg)
+        },
+        updated(){
+            console.log('message is change to '+this.msg)      
         }
-
     }
-    Vue.createApp(app).mount('#app')
+    var mountedApp = Vue.createApp(app).mount('#app')
